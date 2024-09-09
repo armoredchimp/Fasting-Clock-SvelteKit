07e6c5bc471adca104cba2c1aef3ce37dc47b72d
Matt K <mattkasmir@gmail.com>
2024-09-09 09:49:31 -0700

    first deployment

---
+++ src/lib/amplifyConfig.js
@@ -0,0 +1,19 @@
+// import { cognitoData } from "../aws/amplify";
+
+const amplifyConfig = {
+	Auth: {
+		Cognito: {
+			userPoolId: process.env.USER_POOL_ID,
+			userPoolClientId: process.env.USER_POOL_CLIENT_ID,
+			passwordFormat: {
+				minLength: 8,
+				requireLowercase: true,
+				requireUppercase: true,
+				requireNumbers: true,
+				requireSpecialCharacters: true
+			}
+		}
+	}
+};
+
+export default amplifyConfig;
