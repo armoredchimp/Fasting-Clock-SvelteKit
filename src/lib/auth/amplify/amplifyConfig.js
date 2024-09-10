fcc5a4aab22005cbc27f806df3205c5a4c3a2c6e
Matt K <mattkasmir@gmail.com>
2024-09-10 10:09:15 -0700

    attempt 5

--- src/lib/auth/amplify/amplifyConfig.js
+++ src/lib/auth/amplify/amplifyConfig.js
@@ -1,8 +1,10 @@
 // import { cognitoData } from "../aws/amplify";
 // import { Amplify } from "aws-amplify";
-// import dotenv from 'dotenv'
-const userPoolId = process.env['USER_POOL_ID']
-const userPoolClientId = process.env['USER_POOL_CLIENT_ID']
+import { env } from '$env/dynamic/private'
+
+
+const userPoolId = env['USER_POOL_ID']
+const userPoolClientId = env['USER_POOL_CLIENT_ID']
 
 const amplifyConfig = {
 	Auth: {
